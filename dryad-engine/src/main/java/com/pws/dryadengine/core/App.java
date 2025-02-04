357ae0764ee6712c6512826e3e7734b8d75fa8a8
Ciprian C <cipthebull@gmail.com>
2025-02-04 14:01:33 +0100

    snapshot 0402_1400

--- dryad-engine/src/main/java/com/pws/dryadengine/core/App.java
+++ dryad-engine/src/main/java/com/pws/dryadengine/core/App.java
@@ -3,10 +3,11 @@ package main.java.com.pws.dryadengine.core;
 import main.java.com.pws.dryadengine.func.LogsManager;
 
 public class App {
-    private static final String version = "0.1.2";
+    @SuppressWarnings("unused")
+    private static final String version = "0.1.3";
+
     public static volatile boolean finnishExecution = false;
     public static final String saveFileFolder = "data/saves/";
-
     public static final Thread backend = new Thread(new BackendEnv());
     public static final Thread frontend = new Thread(new FrontendEnv());
     
@@ -26,5 +27,4 @@ public class App {
             LogsManager.logErrors(e);
         }
     }
-
 }
